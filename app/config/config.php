7aa895bbf52179f2eb98d5fe4ed66e9345c9389e
Ryan Adi Saputra <158378604+DreamlandSR@users.noreply.github.com>
2025-01-07 12:52:54 +0700

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/SMK7/public');
+
+//Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'e_learning');
